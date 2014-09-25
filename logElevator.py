26a7de8fc0b8d885617fd151f5c2f59886212d71
Luc Mazardo <luc@mazardo.com>
2014-09-26 00:47:42 +0200

    add tests

--- logElevator.py
+++ logElevator.py
@@ -1,34 +1,26 @@
 from flask import Flask
 from flask import request
 
-current_floor=0
 app = Flask(__name__)
+
+import SimpleElevator
+elevator = SimpleElevator.SimpleElevator()
 
 @app.route("/nextCommand")
 def nextCommand():
-    global current_floor
-    if current_floor != 5:
-        current_floor = current_floor + 1
-        return "UP"
-    if current_floor == 5:
-        current_floor -= 1
-        return "DOWN"
-    return "NOTHING"
-    #return "UP|DOWN|OPEN|CLOSE|NOTHING"
+    global elevator
+    return elevator.next_command()
 
 @app.route("/call")
 def call():
-    print "call at "+ request.args.get("atFloor") + " to " + request.args.get("to")
-    return ""
+    global elevator
+    return elevator.call(request.args.get("atFloor"), request.args.get("to"))
 
 @app.route("/reset")
 def reset():
-    global current_floor
-    current_floor=0
-    print "reset : " + request.args.get("cause")
-    return ""
+    global elevator
+    return elevator.reset(request.args.get("cause"))
 
 if __name__ == "__main__":
-    current_floor=0
     app.run(debug=True)
 
